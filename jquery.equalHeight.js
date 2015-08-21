fcb6d0a39342fbeba3c19e445bc51258b9b5c92b
richard dows <jbbrwcky@gmail.com>
2015-08-21 14:13:43 +0100

    equalHeight.js
    makes boxes equal heights, jquery

---
+++ jquery.equalHeight.js
@@ -0,0 +1,15 @@
+// JavaScript Document  
+(function($){
+    $.fn.equalHeight = function() {
+      tallest = 0;
+      this.each(function() {
+        thisHeight = $(this).height();
+        if( thisHeight > tallest)
+         tallest = thisHeight;
+      });
+
+      this.each(function() {
+        $(this).height(tallest);
+      });
+    }
+  })(jQuery);
